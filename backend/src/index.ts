6c6e0e52410022a734a955cd41f9baf7d1eec52f
Manav Bansal <manav.bansal.hsr@gmail.com>
2024-02-16 14:01:44 +0530

    Integrate MongoDB Database Connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,12 @@
-import express from "express";
-
-const app = express();
-
-//  middlewares
-app.use(express.json());
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
 
 //  connections and listeners
-app.listen(5000 , () => {
-    console.log("Server Open");
-})
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server Open and Connected to MongoDB");
+    });
+  })
+  .catch((err) => console.log(err));
