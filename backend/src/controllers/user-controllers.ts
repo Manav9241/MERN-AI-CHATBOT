da778756e1b936bb095d286f621e83d283c47074
Manav Bansal <manav.bansal.hsr@gmail.com>
2024-02-17 11:38:52 +0530

    user model created and 1st request created

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,3 +1,12 @@
-export const getAllUsers = () => {
-    //  get all users from the db
+import { NextFunction, Request, Response } from "express";
+import User from "../models/user-model.js";
+
+export const getAllUsers = async (req: Request , res: Response , next: NextFunction) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({message:"OK" , users});
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({message: "Error" , cause: error.message });
+    }
 }
