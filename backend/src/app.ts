e2409e41bb6e3e2a3a848c73b95771eab6b66506
Manav Bansal <manav.bansal.hsr@gmail.com>
2024-02-18 18:06:16 +0530

    Tokens added to the login and sigup functionality

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,13 @@ import express from "express";
 import { config } from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
 config();
 const app = express();
 
 //  middlewares
 app.use(express.json());
-
+app.use(cookieParser(process.env.COOKIE_SECRET));
 //  only use this in development mode and delete in production mode
 app.use(morgan("dev"));
 
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { compare, hash } from "bcrypt";
 import User from "../models/user-model.js";
+import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 
 export const getAllUsers = async (
   req: Request,
@@ -34,10 +36,30 @@ export const userSignup = async (
     const hashedPassword = await hash(password, 10);
 
     const user = new User({ name, email, password: hashedPassword });
-    await user
-      .save()
-      .then(() => console.log("User saved into the database"))
-      .catch((err) => console.log(err));
+    await user.save()
+    .then(() => console.log("User saved into the database"))
+    .catch((err) => console.log(err));
+    
+    //  create token and store cookie
+
+    res.clearCookie(COOKIE_NAME , {
+        path: '/',
+        domain: 'localhost',
+        httpOnly: true,
+        signed: true
+    })
+
+    const token = createToken(user._id.toString() , user.email , "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() +7);
+    res.cookie(COOKIE_NAME , token , {
+        path: '/',
+        domain: 'localhost',
+        expires,
+        httpOnly: true,
+        signed: true
+    })
+
 
     return res.status(201).json({ message: "OK", id: user._id.toString() });
   } catch (error) {
@@ -64,6 +86,27 @@ export const userLogin = async (
     if (!isPasswordCorrect) {
       return res.status(403).send("Incorrect Password");
     }
+
+    //  create token and store cookie
+
+
+    res.clearCookie(COOKIE_NAME , {
+        path: '/',
+        domain: 'localhost',
+        httpOnly: true,
+        signed: true
+    })
+
+    const token = createToken(user._id.toString() , user.email , "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() +7);
+    res.cookie(COOKIE_NAME , token , {
+        path: '/',
+        domain: 'localhost',
+        expires,
+        httpOnly: true,
+        signed: true
+    })
 
     return res.status(200).json({ message: "OK", id: user._id.toString() });
   } catch (error) {
