bf5a71e02dc291a4eadcfc19961fdd0b993d28d0
Manav Bansal <manav.bansal.hsr@gmail.com>
2024-03-10 19:26:51 +0530

    Navigation links added to the navbar

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,51 @@
 import { AppBar, Toolbar } from "@mui/material";
 import React from "react";
 import Logo from "./shared/logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLink from "./shared/NavigationLink";
 
 const Header = () => {
+  const auth = useAuth();
   return (
     <AppBar
       sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
     >
       <Toolbar sx={{ display: "flex" }}>
         <Logo />
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLink
+                to="/chat"
+                bg="#00fffc"
+                text="Go To Chat"
+                textColor="black"
+              />
+              <NavigationLink
+                to="/"
+                bg="#51538f"
+                text="Logout"
+                textColor="white"
+                onClick={auth.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLink
+                to="/login"
+                bg="#00fffc"
+                text="Login"
+                textColor="black"
+              />
+              <NavigationLink
+                to="/signup"
+                bg="#51538f"
+                text="Signup"
+                textColor="white"
+              />
+            </>
+          )}
+        </div>
       </Toolbar>
     </AppBar>
   );
